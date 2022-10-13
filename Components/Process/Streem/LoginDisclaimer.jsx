eaaa00ef0284267bb7c66b35e0ac9ed3232253ba
monjurctg <monjurpy.06@gmail.com>
2022-10-13 21:27:05 +0600

    update

--- Components/Process/Streem/LoginDisclaimer.jsx
+++ Components/Process/Streem/LoginDisclaimer.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import CheckBox from "../../UI/CustomTag/CheckBox";
 import Text from "../../UI/CustomTag/Text";
 
 function LoginDisclaimer({onNext}) {
+  const [check, setCheck] = useState(false);
+  console.log(check);
   return (
     <div className="process-container">
       <Text fs={31} fw={400} color="#9DB4D3">
@@ -10,7 +12,7 @@ function LoginDisclaimer({onNext}) {
       </Text>
       <div className="process-body">
         <div className="condition">
-          <CheckBox />
+          <CheckBox isCheck={check} setIsCheck={setCheck} />
           <p className="p">I agree that I am 18 years old.</p>
         </div>
       </div>
--- Components/UI/CustomTag/CheckBox.jsx
+++ Components/UI/CustomTag/CheckBox.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function CheckBox() {
+  const [isCheck, setIsCheck] = useState(false);
+
+  const handleChange = (e) => {
+    setIsCheck(e.target.checked);
+  };
   return (
     <label class="checkbox-container">
-      <input type="checkbox" checked="checked" />
+      <input onChange={handleChange} type="checkbox" checked={isCheck} />
       <span class="checkmark"></span>
     </label>
   );
